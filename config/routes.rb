80816f989a25f3e662264bb80086be8a8b838d4c
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-02-14 12:17:55 -0300

    Criação de página inicial do sistema, com uma breve apresentação da finalidade, assim como ajustes de botões das páginas. Configuração das rotas da aplicação

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "deputados#selecao_estado"
-  
+  root "home#index"
+
+  get "/selecao_estado", to: "deputados#selecao_estado"
   get "/deputados", to: "deputados#index"
 end
