613bc019804473c6ec805a2fec68bf0ae3777a76
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-02-14 15:42:21 -0300

    Configuração do bootstrap, para melhor visualização das informações na tela

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @total_deputados = Deputado.count
+    @total_despesas = Despesa.count
+    @gastos_totais = Despesa.sum(:valor_liquido)
+
+    @deputados_por_estado = Deputado.group(:estado).order(:estado).count
+    @gastos_por_estado = Despesa.joins(:deputado).group("deputados.estado").sum(:valor_liquido)
   end
 end
