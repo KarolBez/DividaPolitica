a621c49191619bb5da723a8a2fd967b032199545
Guilherme Rocha <jguilherme.1997@gmail.com>
2025-02-21 19:56:16 -0300

    incluso filtro da consulta dos exercícios dos gastos políticos

--- app/controllers/deputados_controller.rb
+++ app/controllers/deputados_controller.rb
@@ -5,12 +5,14 @@ class DeputadosController < ApplicationController
   def index
     if params[:estado].present?
       @deputados = Deputado
-        .select("deputados.*, COALESCE(SUM(despesas.valor_liquido), 0) AS total_despesas")
+        .select("deputados.*, COALESCE(SUM(despesas.valor_liquido), 0) AS total_despesas, MAX(valor_liquido) as maior_despesa")
         .joins("LEFT JOIN despesas ON despesas.deputado_id = deputados.id")
         .where(estado: params[:estado])
         .group("deputados.id")
         .order("total_despesas DESC")
-      
+
+      @deputados = @deputados.where("EXTRACT(YEAR FROM despesas.data_emissao) = ?", params[:ano].to_i) if params[:ano].present?
+
       @deputados = @deputados.where("LOWER(nome) LIKE ?", "%#{params[:nome].downcase}%") if params[:nome].present?
 
       @deputados = @deputados.where(partido: params[:partido]) if params[:partido].present?
@@ -28,3 +30,6 @@ class DeputadosController < ApplicationController
     @total_despesas = @despesas.sum(:valor_liquido)
   end
 end
+
+
+Despesa.select("EXTRACT(YEAR FROM data_emissao)").where("EXTRACT(YEAR FROM data_emissao) > 2000")
